0293187e72c0d1b5ffa43485966829665f8c7bc9
scotty <87974560+ScottN13@users.noreply.github.com>
2025-11-19 17:09:00 +0200

    patch 1

--- start.sh
+++ start.sh
@@ -1,4 +1,16 @@
 sudo apt update
 sudo apt upgrade -y python3
 
+## Check to see if there are any new changes to main repo (ScottN13/spawncamped)
+git fetch origin
+LOCAL=$(git rev-parse HEAD)
+REMOTE=$(git rev-parse origin/main)
+if [ $LOCAL != $REMOTE ]; then
+    echo "New update found. Updating code..."
+    git pull origin main
+else
+    echo "No updates found. Continuing to start the bot."
+fi
+
+
 python3 main.py
